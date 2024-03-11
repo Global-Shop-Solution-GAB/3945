9e232ff1936e1662f5fc9864ba98a4bdde931887
Service Web <sw@gssmail.com>
2024-03-11 10:01:04 -0500

    Commit changes in C:\Websites\Git\emarlow\3945\GCG_3945_PO_REQ_TEXT.sql

---
+++ GCG_3945_PO_REQ_TEXT.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_3945_PO_REQ_TEXT
+ * ========================================== */
+CREATE TABLE "GCG_3945_PO_REQ_TEXT"(
+ "LINE" CHAR(4),
+ "NEW_RFQ" CHAR(6),
+ "OLD_REQ" CHAR(6),
+ "OLD_LINE" CHAR(4));
+
+
